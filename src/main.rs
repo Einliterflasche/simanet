36a9c23b446aee079c07cf384bdab02f9aa052c5
Einliterflasche <einliterflasche@pm.me>
2023-03-10 23:26:02 +0100

    First contact

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use std::thread;
+use std::sync::mpsc::Sender;
+
+use crate::net::{Connectable, IpPacket};
+
 mod net;
 
+use net::Host;
+
 fn main() {
+    let hosts: Vec<Host> = vec![Host::new(), Host::new()];
+
+    let sender: Sender<IpPacket> = hosts[0].connect();
+
+    thread::spawn(move || {
+        println!("Sending packet");
+        sender.send(IpPacket::default()).unwrap()
+    }).join().unwrap();
+
     println!("Hello, world!");
 }
--- src/net.rs
+++ src/net.rs
@@ -1,33 +1,72 @@
-use std::sync::mpsc::Sender;
+use std::{sync::mpsc::{Sender, Receiver, channel}, thread, marker};
 
+#[derive(Default)]
 pub struct IpPacket {
     header: IpHeader,
     data: Vec<u8>
 }
 
+#[derive(Default)]
 struct IpHeader {
-    data: [u8; 24]
+    data: Vec<u32>
 }
 
 impl IpHeader {
     pub fn source_addr(&self) -> u32 {
         let mut addr = 0;
-        for i in 12..15 {
-            addr += (self.data[i] as u32) << 8;
+        for i in 12..16 {
+            addr += self.data[i] << 8;
+        };
+
+        addr
+    }
+
+    pub fn dest_addr(&self) -> u32 {
+        let mut addr = 0;
+        for i in 16..20 {
+            addr += self.data[i] << 8;
         };
 
         addr
     }
 }
 
-pub trait Connectable {
-    fn send_ip(&self) -> Sender<IpPacket>;
+pub trait Connectable<Prot> {
+    fn connect(&self) -> Sender<Prot>;
 }
 
-pub struct Host {}
+pub struct Host {
+    ip_port: Port<IpPacket>
+}
 
-impl Connectable for Host {
-    fn send_ip(&self) -> Sender<IpPacket> {
-        todo!()
+struct Port<Prot> {
+    transmitter: Sender<Prot>
+}
+
+impl<Prot: marker::Send + 'static> Port<Prot> {
+    fn new(tx: Sender<Prot>, rx: Receiver<Prot>) -> Port<Prot> {
+        thread::spawn(move || {
+            rx.recv().unwrap();
+            println!("Port recieved ip packet")
+        });
+
+        Port {
+            transmitter: tx
+        }
+    } 
+}
+
+impl Host {
+    pub fn new() -> Host {
+        let (tx, rx) = channel::<IpPacket>();
+        Host {
+            ip_port: Port::new(tx, rx)
+        }
+    }
+}
+
+impl Connectable<IpPacket> for Host {
+    fn connect(&self) -> Sender<IpPacket> {
+        self.ip_port.transmitter.clone()
     }
 }
